50ae1824b7ceecb592b45520ed6c8c98a8edb108
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2021-09-02 10:19:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,25 +3,25 @@
 
 # Build the AWS Transit 
 
-# module "transit_aws_1" {
-#   source  = "terraform-aviatrix-modules/aws-transit/aviatrix"
-#   version = "v4.0.3"
+module "transit_aws_1" {
+  source  = "terraform-aviatrix-modules/aws-transit/aviatrix"
+  version = "v4.0.3"
 
-#   cidr = "10.77.0.0/20"
-#   region = "eu-central-1"
-#   account = "Eskimoo"
+  cidr = "10.77.0.0/20"
+  region = "eu-central-1"
+  account = "Eskimoo"
 
-#   name = "avffhub"
-#   ha_gw = "false"
-#   instance_size = "t2.micro"
-# }
+  name = "avffhub"
+  ha_gw = "false"
+  instance_size = "t2.micro"
+}
 
 
 # Build the AWS Spoke 1
 
 module "spoke_aws_1" {
   source  = "Eskimoodigital/aws-spoke-ec2/aviatrix"
-  version = "1.0.5"
+  version = "1.0.11"
 
   name            = "avffsp1"
   cidr            = "10.77.16.0/20"
@@ -38,21 +38,21 @@ module "spoke_aws_1" {
 
 # Build the AWS Spoke 2
 
-# module "spoke_aws_2" {
-#   source  = "terraform-aviatrix-modules/aws-spoke/aviatrix"
-#   version = "4.0.3"
+module "spoke_aws_2" {
+  source  = "Eskimoodigital/aws-spoke-ec2/aviatrix"
+  version = "1.0.11"
 
-#   name            = "avffsp2"
-#   cidr            = "10.77.32.0/20"
-#   region          = "eu-central-1"
-#   account         = "Eskimoo"
-#   transit_gw      = "avx-avffhub-transit"
+  name            = "avffsp2"
+  cidr            = "10.77.32.0/20"
+  region          = "eu-central-1"
+  account         = "Eskimoo"
+  transit_gw      = "avx-avffhub-transit"
   
   
 
-#   ha_gw = "false"
-#   instance_size = "t2.micro"
-# }
+  ha_gw = "false"
+  instance_size = "t2.micro"
+}
 
   
   
